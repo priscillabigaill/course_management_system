1fcec3a09c1bd80002c26162aaa54fa9a2fec843
Abigail <91783712+priscillabigaill@users.noreply.github.com>
2023-05-29 21:06:51 +0700

    Update Benchmark.java

--- Benchmark.java
+++ Benchmark.java
@@ -1,12 +1,11 @@
 public class Benchmark {
-
+                
+        // trial
 
         public static void main(String[] args) {
             long startTime = System.currentTimeMillis();
 
-            // Perform your operations
-
-            //
+            // BST
             BinarySearchTree test = new BinarySearchTree();
 
             // add course
@@ -15,25 +14,23 @@ public class Benchmark {
             test.addCourse("History", "3456", "Wednesday", "12.00", "14.00", "Audrey");
             test.addCourse("Economics", "4567", "Thursday", "12.00", "14.00", "Lenno");
             test.addCourse("Biology", "5678", "Friday", "12.00", "14.00", "Randy");
-
             // remove course
             test.removeCourse("Economics");
-
             // modify course
             test.modifyCourse("Chemistry","Physics", "7777", "Thursday", "12.00", "14.00", "Aubert");
-
             // view course
             test.viewCourses();
-
             // search course by name
             test.searchCourseByName("Math");
-
+            test.searchCourseByName("Biology");
+            test.searchCourseByName("Physics");
+            test.searchCourseByName("History");
             // add student
-            test.addStudent("Abigail", "7777", "2345");
+            test.addStudent("Abigail", "3532", "7777");
             test.addStudent("Jeffrey", "7887", "3456");
-
             // remove student
-
+            test.removeStudent("7777", "3532");
+            test.removeStudent("3456", "7887");
 
             long endTime = System.currentTimeMillis();
             long elapsedTime = endTime - startTime;
